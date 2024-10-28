a29316027c7c005236d62b0e53ceb866eb465c57
AnonymousAcid <nilsonjvidana@gmail.com>
2024-10-27 18:41:31 -0700

    Add SalvaPhysicsPlugin to app

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 use bevy::{app::{App, Startup}, prelude::{Camera3dBundle, Commands, Transform}, DefaultPlugins};
 use bevy_flycam::{FlyCam, NoCameraPlayerPlugin};
 use bevy_rapier3d::{plugin::RapierPhysicsPlugin, prelude::{Collider, RigidBody}, render::RapierDebugRenderPlugin};
+use plugin::SalvaPhysicsPlugin;
+use salva3d::solver::DFSPHSolver;
+
+mod plugin;
 
 fn main() {
     let mut app = App::new();
 
+    let fluid_solver: DFSPHSolver = DFSPHSolver::new();
     app.add_plugins((
         DefaultPlugins,
         RapierPhysicsPlugin::<()>::default(),
         RapierDebugRenderPlugin::default(),
+        SalvaPhysicsPlugin::new(fluid_solver),
         NoCameraPlayerPlugin
     ));
 
