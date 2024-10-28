8e420971640ae2c79a0624cfa9a711cd0c96f3b5
AnonymousAcid <nilsonjvidana@gmail.com>
2024-10-27 18:40:01 -0700

    Create app with default plugins & rapier physics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use bevy::{app::{App, Startup}, prelude::{Camera3dBundle, Commands, Transform}, DefaultPlugins};
+use bevy_flycam::{FlyCam, NoCameraPlayerPlugin};
+use bevy_rapier3d::{plugin::RapierPhysicsPlugin, prelude::{Collider, RigidBody}, render::RapierDebugRenderPlugin};
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+
+    app.add_plugins((
+        DefaultPlugins,
+        RapierPhysicsPlugin::<()>::default(),
+        RapierDebugRenderPlugin::default(),
+        NoCameraPlayerPlugin
+    ));
+
+    app.add_systems(Startup, startup);
+
+    app.run();
+}
+
+fn startup(
+    mut commands: Commands
+) {
+    commands.spawn((
+        Camera3dBundle::default(),
+        FlyCam
+    ));
+
+    commands.spawn((
+        RigidBody::Fixed,
+        Collider::cuboid(10., 0.1, 10.),
+        Transform::from_xyz(0., -0.1, 0.)
+    ));
 }
