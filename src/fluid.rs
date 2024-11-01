bcb921dec16940d63fddcc9b75f4a9126ee410de
Nilson J Vidanapatiranage <nilsonjvidana@gmail.com>
2024-11-01 08:43:14 -0700

    Create FluidParticlePositions and FluidDensity components

--- src/fluid.rs
+++ src/fluid.rs
@@ -6,37 +6,18 @@ use salva3d::{math::{Point, Real}, object::FluidHandle};
 #[derive(Component)]
 pub struct SalvaFluidHandle(pub FluidHandle);
 
+
 #[derive(Component)]
-pub struct Fluid {
-    raw: salva3d::object::Fluid
+pub struct FluidParticlePositions {
+    pub positions: Vec<Vec3>
 }
 
-impl Fluid {
-    pub fn new(
-        particle_positions: Vec<Vec3>,
-        particle_radius: Real, // XXX: remove this parameter since it is already defined by the liquid world.
-        density0: Real,
-    ) -> Self {
-        let salva_particle_positions = particle_positions.iter()
-            .map(|pt| { Point::new(pt.x, pt.y, pt.z) })
-            .collect();
+/// The rest density of a fluid (default 1000.0)
+#[derive(Component)]
+pub struct FluidDensity { pub density0: Real }
 
-        Self {
-            raw: salva3d::object::Fluid::new(salva_particle_positions, particle_radius, density0)
-        }
-    }
-}
-
-impl Deref for Fluid {
-    type Target = salva3d::object::Fluid;
-
-    fn deref(&self) -> &Self::Target {
-        &self.raw
-    }
-}
-
-impl DerefMut for Fluid {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.raw
+impl Default for FluidDensity {
+    fn default() -> Self {
+        Self { density0: 1000.0 }
     }
 }
